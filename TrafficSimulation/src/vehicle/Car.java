c70a490fef6728e866aef0884faa39590931b44d
cookda <cookda@appstate.edu>
2017-01-30 17:52:55 -0500

    Testing stuff

---
+++ TrafficSimulation/src/vehicle/Car.java
@@ -0,0 +1,9 @@
+package vehicle;
+
+import vehicle.impl.Vehicle;
+
+/**
+ * Created by gigaw on 1/30/2017.
+ */
+public class Car extends Vehicle {
+}
---
+++ TrafficSimulation/src/vehicle/impl/Vehicle.java
@@ -0,0 +1,7 @@
+package vehicle.impl;
+
+/**
+ * Created by gigaw on 1/30/2017.
+ */
+public abstract class Vehicle {
+}
