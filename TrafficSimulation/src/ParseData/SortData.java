20017a0a77cfc7686d2951acdecbf63d83aeaaab
cookda <cookda@appstate.edu>
2017-02-10 11:40:13 -0500

    Testing stuff

---
+++ TrafficSimulation/src/ParseData/SortData.java
@@ -0,0 +1,13 @@
+package ParseData;
+
+import java.io.File;
+
+/**
+ * Created by gigaw on 2/10/2017.
+ */
+public class SortData {
+    void readFile(string fileName){
+        File dataFile = new File(fileName);
+
+    }
+}
