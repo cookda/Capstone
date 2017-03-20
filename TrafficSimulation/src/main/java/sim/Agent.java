392517b010bd6d989e83ee889010bf638a77debe
arogore <goreaw@appstate.edu>
2017-03-20 17:04:18 -0400

    Added new constructor and trip var

--- TrafficSimulation/src/main/java/sim/Agent.java
+++ TrafficSimulation/src/main/java/sim/Agent.java
@@ -1,5 +1,6 @@
 package sim;
 
+import org.antlr.v4.runtime.misc.Pair;
 import org.jxmapviewer.viewer.GeoPosition;
 
 /**
@@ -10,11 +11,18 @@ public class Agent {
     private double latitude;
     private double longitude;
     private GeoPosition geoPosition;
+    private Pair<GeoPosition, GeoPosition> trip;
 
     public Agent(double latitude, double longitude) {
         this.latitude = latitude;
         this.longitude = longitude;
         geoPosition = new GeoPosition(latitude, longitude);
+    }
+
+    public Agent(double latitude, double longitude, GeoPosition start, GeoPosition end) {
+        this(latitude, longitude);
+        trip = new Pair<>(start, end);
+
     }
 
     public double getLatitude() {
@@ -36,4 +44,17 @@ public class Agent {
     public GeoPosition getGeoPosition() {
         return geoPosition;
     }
+
+    public void setGeoPosition(GeoPosition geoPosition) {
+        this.geoPosition = geoPosition;
+    }
+
+    public Pair<GeoPosition, GeoPosition> getTrip() {
+        return trip;
+    }
+
+    public void setTrip(Pair<GeoPosition, GeoPosition> trip) {
+        this.trip = trip;
+    }
+
 }
