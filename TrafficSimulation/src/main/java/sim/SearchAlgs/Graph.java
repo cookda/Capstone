e2bddb7962652354e3c914effb6991dc5ec1233d
cookda <cookda@appstate.edu>
2017-03-24 13:21:43 -0400

    Made the haversine and started to implement the search algorithms

--- TrafficSimulation/src/main/java/sim/SearchAlgs/Graph.java
+++ TrafficSimulation/src/main/java/sim/SearchAlgs/Graph.java
@@ -3,9 +3,11 @@ package sim.SearchAlgs;
 import core.UserProfile;
 import nodes.impl.TNode;
 import nodes.impl.Way;
+import sim.Agent;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 
 /**
  * Created by Dalton on 3/24/2017.
@@ -15,6 +17,7 @@ public class Graph {
     private HashMap<Long, TNode> nodeMap;
     private static Graph instance;
     private ArrayList<TNode> ways;
+    private HashSet<GNode> graph;
 
 
     public static Graph getInstance(){
@@ -34,9 +37,27 @@ public class Graph {
         wayMap.values().forEach(way -> {
             ways = way.getNodes();
             for(int i = 0; i < ways.size(); i++){
+                TNode left = ways.get(i);
+                TNode right = ways.get(i + 1);
+                double distance = getDistance(left, right);
+                Edge e = new Edge(distance, left, right);
+                GNode g = new GNode(left, e);
+                if(!graph.contains(g)){
+                    graph.add(g);
+                }
+                else{
 
+                }
             }
         });
+    }
+
+    public double getDistance(TNode one, TNode two){
+        double latStart = one.getLat();
+        double lonStart = one.getLon();
+        double latEnd = two.getLat();
+        double lonEnd = two.getLon();
+        return Agent.haverSine(latStart, lonStart, latEnd, lonEnd);
     }
 
 
