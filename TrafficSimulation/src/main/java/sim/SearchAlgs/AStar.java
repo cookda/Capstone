3c77e2ca1578c6aa1bdd503e9ca8f13bdea64351
arogore <arongore@gmail.com>
2017-04-22 10:44:04 -0400

    Fixed a*

--- TrafficSimulation/src/main/java/sim/SearchAlgs/AStar.java
+++ TrafficSimulation/src/main/java/sim/SearchAlgs/AStar.java
@@ -20,17 +20,11 @@ import java.util.*;
  * http://mat.uab.cat/~alseda/MasterOpt/AStar-Algorithm.pdf : PsuedoCode
  */
 public class AStar extends SearchAlg{
-    Graph<TNode, DefaultEdge> graph;
-    ArrayList<GNode> path;
-    Pair<GeoPosition, GeoPosition> trip;
-    TNode start;
-    TNode end;
-    ArrayList<TNode> open = new ArrayList<TNode>(); //both the open and closed are used in get Path (A*)
-    ArrayList<TNode> closed = new ArrayList<TNode>();
-    private List<TNode> adj;
-    private TNode nextNode;
+    private Graph<TNode, DefaultEdge> graph;
+    private Pair<GeoPosition, GeoPosition> trip;
+    private TNode start;
+    private TNode end;
 
-    public double searchDistance(){return 0.0;}
 
     public AStar(Agent agent){
         graph = GraphSim.getInstance().getGraph();
@@ -39,32 +33,14 @@ public class AStar extends SearchAlg{
     }
 
     public void findTripNodes(){
-        GeoPosition startNode = trip.getKey();
-        GeoPosition endNode = trip.getValue();
-        double latStart = startNode.getLatitude();
-        double lonStart = startNode.getLongitude();
-        double latEnd = endNode.getLatitude();
-        double lonEnd = endNode.getLongitude();
         Set<TNode> vertices = graph.vertexSet(); //graph size is 0 not good
-        System.out.println("Size of graph " + vertices.size());
-        TNode[] nodes = vertices.toArray(new TNode[vertices.size()]);
-        for(TNode vert : nodes){
-            System.out.println(vert.getId());
-            if(vert.getLat() == latStart && vert.getLon() == lonStart)
-                System.out.println("Found Start");
-                start = vert;
-            if(vert.getLon() == latEnd && vert.getLon() == lonEnd)
-                System.out.println("Found End");
-                end = vert;
+        for (TNode node : vertices) {
+            if (node.getGeoPosition().equals(trip.getKey())) {
+                start = node;
+            } else if (node.getGeoPosition().equals(trip.getValue())) {
+                end = node;
+            }
         }
-        /*Optional<TNode> nodeOptional = graph.stream().filter(node ->
-            node.getLat() == latStart && node.getLon() == lonStart
-        ).findFirst();
-        Optional<TNode> nodeOptional2 = graph.stream().filter(node ->
-            node.getLat() == latEnd && node.getLon() == lonEnd
-        ).findFirst();
-        start = nodeOptional.orElse(null);
-        end = nodeOptional2.orElse(null);*/
     }
 
     /**
@@ -73,7 +49,6 @@ public class AStar extends SearchAlg{
      * to the current node and take the best one according to distance to the goal node.
      */
     public TNode[] getPath(){
-
         AStarShortestPath<TNode, DefaultEdge> path = new AStarShortestPath<>(graph, ((tNode, v1) -> {
                 double R = 6372.8;
                 double lonEnd = v1.getLon();
@@ -98,67 +73,5 @@ public class AStar extends SearchAlg{
         GraphPath<TNode, DefaultEdge> answer = path.getPath(start, end);
         List<TNode> hopeIsNotLost = answer.getVertexList();
         return hopeIsNotLost.toArray(new TNode[hopeIsNotLost.size()]);
-        /*TNode current = start;
-        open.add(current);
-        path.add(start);
-        while(!open.isEmpty()){
-            TNode nextNode = determineNode(current);
-
-            path.add(nextNode);
-            //System.out.print(nextNode.getLat() + " " + nextNode.getLon() + "\n");
-            System.out.println("This is the the end goal:" + end.getLat() + " " + end.getLon() + "\n");
-            current = nextNode;
-            System.out.println(current.getLat() + " " + current.getLon());
-            if(current.equals(end)){
-                break;
-            }
-        }
-        return path;*/
     }
-
-    public TNode getStart(){
-        return start;
-    }
-
-    public TNode getEnd(){
-        return end;
-    }
-
-    /*private TNode determineNode(TNode current){
-        adj = getAdjacent(current);
-        nextNode = null;
-        double cost = Double.MAX_VALUE;
-        for (int i = 0; i < adj.size(); i++) {
-            double newCost = 0.0;
-            newCost = Graph.getDistance(current, adj.get(i));
-            newCost += Graph.getDistance(adj.get(i), end);
-            if (newCost < cost && !closed.contains(adj.get(i))) {
-                cost = newCost;
-                nextNode = adj.get(i);
-                open.remove(adj.get(i));
-                closed.add(adj.get(i));
-            } else if (i == adj.size() - 1 && closed.contains(adj.get(i))) {
-                for (int j = 0 ; j < open.size(); j++){
-                    if(open.get(i) != null){
-                        current = open.get(i);
-                        break;
-                    }
-                }
-                break;
-
-            } else {
-                open.remove(adj.get(i));
-                closed.add(adj.get(i));
-            }
-        }
-        return nextNode;
-    }
-    private ArrayList<TNode> getAdjacent(TNode start){
-        ArrayList<TNode> adj = new ArrayList<TNode>();
-        for(Edge e: start.getEdges()){
-            adj.add(e.getEnd());
-            open.add(e.getEnd());  //recently added
-        }
-        return adj;
-    }*/
 }
